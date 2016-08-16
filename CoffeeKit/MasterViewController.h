34719b04e94f2572914a7c807a8316368e576e2d
Swarup_Pattnaik <pattnaik.swarup@gmail.com>
2016-08-17 01:08:02 +0530

    Initial Commit

---
+++ CoffeeKit/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  CoffeeKit
+//
+//  Created by Swarup_Pattnaik on 17/08/16.
+//  Copyright © 2016 Swarup_Pattnaik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
